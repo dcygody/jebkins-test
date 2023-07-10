21dd54c5e75d5c38fb2efd62852e69a6cd43a68e
dcy <1329851034@qq.com>
2023-07-10 16:08:47 +0800

    初始化提交

---
+++ src/main/java/cn/zing/Application.java
@@ -0,0 +1,19 @@
+package cn.zing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: dcy
+ * @create: 2023-07-10 16:01
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
+
+
---
+++ src/main/java/cn/zing/controller/TestController.java
@@ -0,0 +1,21 @@
+package cn.zing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: dcy
+ * @create: 2023-07-10 16:02
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+
+        return "Success";
+    }
+}
+
+
